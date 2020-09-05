1926f218ecc899055542b7566889892f4da54819
mg143pavankumar <pavansharma.mg143@gmail.com>
2020-09-05 12:30:11 +0530

    Updates

---
+++ precache-manifest.491ec369ce1bcccfd9281016c04e3615.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9c328d37099ea1282f56169455274a",
+    "url": "/reactjs_website/index.html"
+  },
+  {
+    "revision": "32f38e4971452f7ff127",
+    "url": "/reactjs_website/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3b9de61db0bf0010e5e3",
+    "url": "/reactjs_website/static/css/main.f1fe9167.chunk.css"
+  },
+  {
+    "revision": "32f38e4971452f7ff127",
+    "url": "/reactjs_website/static/js/2.3ab3d5af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjs_website/static/js/2.3ab3d5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b9de61db0bf0010e5e3",
+    "url": "/reactjs_website/static/js/main.4ee3a772.chunk.js"
+  },
+  {
+    "revision": "8ccb4a992a3ca86d9b52",
+    "url": "/reactjs_website/static/js/runtime-main.d63a9b77.js"
+  },
+  {
+    "revision": "eb095e43acb175b4181fe7d9ca70b3d1",
+    "url": "/reactjs_website/static/media/banner_bg.eb095e43.jpg"
+  }
+]);
